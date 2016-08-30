3905541f1c48b291b9f268515cc03744536b34cb
Alan Denison and Ben Logue <alan+ben@>
2016-08-30 10:39:39 -0700

    add card images.

---
+++ app/components/card-display.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  model() {
+    return this.store.findAll()
+  },
+  actions: {
+    displayCards(cardArray) {
+
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  dealer: Ember.inject.service(),
   model() {
     return Ember.RSVP.hash({
       users: this.store.findAll('user'),
       cards: this.store.findAll('card'),
-      game: this.store.findAll('game')
+      game: this.store.findAll('game'),
     });
   },
 
